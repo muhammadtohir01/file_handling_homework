ae5be7bce6c8a573df66a483d533ce5b95ca2461
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-11-10 00:08:57 +0500

    5

--- file05.py
+++ file05.py
@@ -7,4 +7,18 @@ def main(data:str):
         list: return answer
     """
     
+    s=0
+    d=0
+    list1=[s,d]
+    for i in data:
+        if i.isdigit():
+            s+=1
+    for i in data:
+        if not(i.isdigit()):
+            d+=1
+        list1=[s,d]
+    return list1
+print(main(open('txt_file/data05.txt','r').read()))
+
+
 # Read data from file
