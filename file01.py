c6cad4e70ac2522a036cc1041bbef6de52ddd317
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-11-09 13:53:55 +0500

    file01.py

--- file01.py
+++ file01.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-
+    list_data = open(data).read()
+    list_str = list_data.split(',')
+    list_int = []
+    for i in list_str:
+        list_int.append(int(i))
+    
+    return list_int
+    
+print(main('txt_file/data01.txt'))
 # Read data from file
